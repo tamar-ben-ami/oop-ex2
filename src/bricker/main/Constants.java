879c76ce13921b6d8499d4c83e22eadc5d753861
yaara.berg <yaara.bergfroind@mail.huji.ac.il>
2024-02-21 20:51:10 +0200

    Constants

--- src/bricker/main/Constants.java
+++ src/bricker/main/Constants.java
@@ -74,20 +74,65 @@ public class Constants {
      * Puck radius factor
      */
     public static final float PUCK_RADIUS_FACTOR = 3/4f;
+    /**
+     * heart dimension
+     */
     public static final Vector2 HEART_DIMENSION = new Vector2(30, 30);
+    /**
+     * window title
+     */
     public static final String WINDOW_TITLE = "Bouncing Ball";
+    /**
+     * Background image
+     */
     public static final String BACKGROUND_IMAGE = "assets/DARK_BG2_small.jpeg";
+    /**
+     * Ball image
+     */
     public static final String BALL_IMAGE = "assets/ball.png";
+    /**
+     * Pucks image
+     */
     public static final String PUCKS_IMAGE = "assets/mockBall.png";
+    /**
+     * Ball collision sound
+     */
     public static final String BALL_COLLISION_SOUND = "assets/blop_cut_silenced.wav";
+    /**
+     * Paddle collision sound
+     */
     public static final String PADDLE_IMAGE = "assets/paddle.png";
+    /**
+     * Paddle collision sound
+     */
     public static final String BORDER_IMAGE = "assets/brick.png";
+    /**
+     * Brick image
+     */
     public static final String BRICK_IMAGE = "assets/brick.png";
+    /**
+     * Brick collision sound
+     */
     public static final String BALL_TAG = "Ball";
+    /**
+     * Puck tag
+     */
     public static final String PUCK_TAG = "Puck";
+    /**
+     * Paddle tag
+     */
     public static final String YOU_LOSE = "You Lose!";
+    /**
+     * Play again?
+     */
     public static final String PLAY_AGAIN = " Play again?";
+    /**
+     * You win!
+     */
     public static final String YOU_WIN = "You win!";
+    /**
+     * Zero constance
+     */
     public static final int ZERO = 0;
 
 }
